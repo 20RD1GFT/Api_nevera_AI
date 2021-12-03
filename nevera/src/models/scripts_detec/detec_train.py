fca2031729d04f287d222aa0e5104fbcba09bbb8
Martínez Mora <matm@gft.com>
2021-12-03 14:27:48 +0100

    cambios

--- nevera/src/models/scripts_detec/detec_train.py
+++ nevera/src/models/scripts_detec/detec_train.py
@@ -10,9 +10,11 @@ record_script = 'Api_nevera_AI/nevera/src/models/scripts_detec/generate_tfrecord
 IMAGE_PATH = 'Api_nevera_AI/nevera/src/train_images'
 
 # generate tfrecords
-!python {record_script} -x {os.path.join(IMAGE_PATH, 'train')} -l {f'{annotations_path}/label_map.pbtxt'} -o {os.path.join(annotations_path, 'train.record')} 
-!python {record_script} -x {os.path.join(IMAGE_PATH, 'test')} -l {f'{annotations_path}/label_map.pbtxt'} -o {os.path.join(annotations_path, 'test.record')} 
+os.system("python {record_script} -x {os.path.join(IMAGE_PATH, 'train')} -l {f'{annotations_path}/label_map.pbtxt'} -o {os.path.join(annotations_path, 'train.record')}")
+os.system("python {record_script} -x {os.path.join(IMAGE_PATH, 'test')} -l {f'{annotations_path}/label_map.pbtxt'} -o {os.path.join(annotations_path, 'test.record')}")
 
 TRAINING_SCRIPT = os.path.join(api_path, 'research', 'object_detection', 'model_main_tf2.py')
 
-command = "python {} --model_dir={} --pipeline_config_path={} --num_train_steps=2000".format(TRAINING_SCRIPT, model_path, pipeline_file)
+command = "python {} --model_dir={} --pipeline_config_path={} --num_train_steps=1".format(TRAINING_SCRIPT, model_path, pipeline_file)
+
+os.system(command)
